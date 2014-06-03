5c567bd20c6b504e1e3ad47402bf68edd6127bcf
Samuli Seppänen <samuli@openvpn.net>
2014-06-03 09:17:57 +0000

    Added class documentation
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,39 @@
 # been implemented yet. Note that you should apply this class _after_ OpenFire 
 # is setup, or monit will start complaining loudly.
 #
+# == Parameters
+#
+# [*chat_allow_addresses_ipv4*]
+#   An array of IPv4 addresses from which to allow client-to-server connections to port 
+#   5222. Defaults to ['127.0.0.1']. 
+# [*chat_allow_addresses_ipv6*]
+#   Same as above but for IPv6. Defaults to ['::1'].
+# [*federate_allow_addresses_ipv4*]
+#   Same as above for IPv4 server-to-server (federation) traffic.
+# [*federate_allow_addresses_ipv6*]
+#   Same as above for IPv6 server-to-server (federation) traffic.
+# [*filetransfer_allow_addresses_ipv4*]
+#   Same as above for IPv4 file transfer proxy traffic.
+# [*filetransfer_allow_addresses_ipv6*]
+#   Same as above for IPv6 file transfer proxy traffic.
+# [*admin_allow_addresses_ipv4*]
+#   An array of IPv4 addresses from which to allow connections to the OpenFire 
+#   administration interface. Defaults to ['127.0.0.1'].
+# [*admin_allow_addresses_ipv6*]
+#   Same as above but for IPv6. Defaults to ['::1'].
+# [*monitor_email*]
+#   Email address where local service monitoring software sends it's reports to.
+#   Defaults to top scope variable $::servermonitor.
+#
+# == Authors
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-lisence
+# See file LICENSE for details
+#
 class openfire
 (
     $chat_allow_addresses_ipv4 = ['127.0.0.1'],
@@ -46,4 +79,3 @@ class openfire
         }
     }
 }
-
